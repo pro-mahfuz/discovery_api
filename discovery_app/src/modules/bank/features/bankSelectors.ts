333845c758018edf498a3fdc60fe6502ad00eba7
pro-mahfuz <pro.mahfuz@gmail.com>
2025-08-14 09:11:54 +0400

    seperate project

--- discovery_app/src/modules/bank/features/bankSelectors.ts
+++
@@ -1,9 +0,0 @@
-import { Bank } from './bankTypes.ts';
-import { RootState } from "../../../store/store.ts";
-
-export const selectBankStatus = (state: RootState) => state.bank.status;
-export const selectBankError = (state: RootState) => state.bank.error;
-
-export const selectAllBank = (state: RootState): Bank[] => state.bank.data || [];
-
-export const selectBankById = (id: number) => (state: RootState) => state.bank.data.find(bank => bank.id === id);
--- discovery_app/src/modules/permission/features/permissionSelectors.ts
+++
@@ -1,5 +0,0 @@
-import { Permission } from './permissionTypes.ts';
-import { RootState } from "../../../store/store.ts";
-
-export const selectAllPermissions = (state: RootState): Permission[] => state.permission.permissions;
-export const selectPermissionStatus = (state: RootState) => state.permission.status;
