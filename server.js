89e5f215007867da36b1e42caf57e17491c4f9b4
pro-mahfuz <pro.mahfuz@gmail.com>
2025-08-14 10:36:10 +0400

    remove port

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 import app from "./app.js";
 import { sequelize } from "./models/model.js";
 
-// app.listen(PORT, () => {
-//   console.log(`Server running ...`);
-// });
+app.listen(() => {
+  console.log(`Server running ...`);
+});
 
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
 
-(async () => {
-  try {
-    // await sequelize.sync({ alter: false });
-    app.listen(PORT, () => {
-      console.log(`Server running at http://localhost:${PORT}`);
-    });
-  } catch (err) {
-    console.error("DB connection error:", err);
-  }
-})();
+// (async () => {
+//   try {
+//     await sequelize.sync({ alter: false });
+//     app.listen(PORT, () => {
+//       console.log(`Server running at http://localhost:${PORT}`);
+//     });
+//   } catch (err) {
+//     console.error("DB connection error:", err);
+//   }
+// })();
